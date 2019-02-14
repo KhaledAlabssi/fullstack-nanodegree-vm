1ed8ac694f088304333b499d0b9836638657af49
KhaledAlabssi <khaled.absi5@gmail.com>
2019-02-14 12:06:40 +0100

    building the database

--- vagrant/catalog/db_setup.py
+++ vagrant/catalog/db_setup.py
@@ -1,5 +1,11 @@
 #  from and import section
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import foreignkey, DateTime, create_engine
+from sqlalchemy.ext.declararive import declarative_base
+from sqlalchemy.orm import relationship, backref
 
+
+Base = declarative_base()
 
 
 
@@ -9,14 +15,10 @@
 class User(Base):
     __tablename__ = 'user'
 
-
-
-
-
-
-
-
-
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    email = Column(String(250), nullable = False)
+    picture = Column(String(250))
 
 # category class as Category with serializetion for json
 #With id, name, user_id with foreignkey and user in relation
@@ -24,23 +26,18 @@ class User(Base):
 
 class Category(Base):
     __tablename__ = 'category'
-
-
-
-
-
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User, backref='category')
+    
     #serialize
     @property
-
-
-
-
-
-
-
-
-
-
+    def serialize(self):
+        return {
+            'name':self.name,
+            'id':self.id
+        }
 
 # items class as Item should contain:
 #id, name, date, description, picture, 
@@ -48,6 +45,16 @@ class Category(Base):
 
 class Items(Base):
     __tablename__ = 'items'
+    id = Cloumn(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    date = Column(DateTime, nullable = False)
+    description = Column(String(250))
+    picture = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category,
+    backref = backref('items', cascade = 'all, delete'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User, backref = 'items')
 
 
 
@@ -57,16 +64,16 @@ class Items(Base):
 
     #serialize
     @property
-
-
+    def serialize(self):
+        return {
+            'name':self.name,
+            'id':self.id,
+            'description':self.description,
+            'picture':self.picture,
+            'category':self.category
+        }
 
 
 #creating the Database
-
-
-
-
-
-
-
-
+engine = create_engine('sqlite:///catalog.db')
+Base.metadat.create_all(engine)
