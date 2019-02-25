e918d9596b307333ef000557f05330d64cee38fa
KhaledAlabssi <khaled.absi5@gmail.com>
2019-02-25 09:07:04 +0100

    Move all to another repo and start freash... 3 days ago

--- vagrant/catalog/README.md
+++ vagrant/catalog/README.md
@@ -1,28 +1,3 @@
-Done:
-
-Been working on a prototype for the project.
-Imports, routes and the gerneral form of the database and app file were done.
-
-
-
-client_secrets: This commit contain the alot of work, The Google json file (client_secrets.json)
-gconnect: Also I finished with the gconnect method but still need to be tested soon after I finish with login.html and catalog.html
-gdisconnect: finish with it and need to be tested as soon as i'm done with html files and gconnect and item_catalog.db tests, May be I will redirected it later to homepage when it possible.
-
-About this Commit:
-Now it's getting complicated, I will get back to work on the categories first and then I will get back to the login part.
-
-
-
-
-To do:
-
-build gconnect, create a project in gconsole and call it Item Catalog.
-write the  application browsing, code the funk of the routes.
-Getting back to html files when I code the process of browsing the app.
-Css and bootstrab 4 after getting html working.
-json for the categories and items as well.
-Still didn't fegure out how to keep only users who are registered to be the one who can crud.
-try to include my website to the project, or eveb about me or so.
-
-
+# Item-Catalog
+Udacity project Item Catalog
+I will try to build an app shows the Auto Brands as Categories for the project and the cars as the Items.... Good luck for me.
--- vagrant/catalog/db_setup.py
+++ vagrant/catalog/db_setup.py
@@ -1,76 +1,60 @@
-#  from and import section
+#import sys
 from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey, DateTime, create_engine
+from sqlalchemy import ForeignKey, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
-
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-
-# user class as User should contain
-# id, name, email and picture
 class User(Base):
     __tablename__ = 'user'
+
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable = False)
-    email = Column(String(250), nullable = False)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
-# category class as Category with serializetion for json
-#With id, name, user_id with foreignkey and user in relation
 
+class Company(Base):
+    __tablename__ = 'company'
 
-class Category(Base):
-    __tablename__ = 'category'
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable = False)
+    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User, backref='category')
-    
-    #serialize
+    user = relationship(User)
+
     @property
     def serialize(self):
+
         return {
-            'name':self.name,
-            'id':self.id
-        }
-
-# items class as Item should contain:
-#id, name, date, description, picture, 
-#category_id FK, category rel, user_id FK and user rel
-
-class Items(Base):
-    __tablename__ = 'items'
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable = False)
-    date = Column(DateTime, nullable = False)
-    description = Column(String(250))
-    picture = Column(String(250))
-    category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(Category,
-    backref = backref('items', cascade = 'all, delete'))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User, backref = 'items')
-
-
-
-
-
-
-
-    #serialize
-    @property
-    def serialize(self):
-        return {
-            'name':self.name,
-            'id':self.id,
-            'description':self.description,
-            'picture':self.picture,
-            'category':self.category
+            'name': self.name,
+            'id': self.id
         }
 
 
-#creating the Database
-engine = create_engine('sqlite:///item_catalog.db')
+class Cars(Base):
+    __tablename__ = 'cars'
+
+    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+    Specifications = Column(String(250))
+    price = Column(String(16))
+    company_id = Column(Integer, ForeignKey('company.id'))
+    company = relationship(Company)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+
+        return {
+            'name': self.name,
+            'Specifications': self.Specifications,
+            'id': self.id,
+            'price': self.price,
+        }
+
+
+
+engine = create_engine('sqlite:///cars.db')
 Base.metadata.create_all(engine)
