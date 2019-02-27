2a7cb30d93634b58f14a0c271e1287ebab645e14
KhaledAlabssi <khaled.absi5@gmail.com>
2019-02-27 10:43:58 +0100

    Add if to edit auto fun and delete requierd from html

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -103,7 +103,7 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print "Token of clien ID doesn't match"
+        # print "Token of clien ID doesn't match"
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -149,7 +149,7 @@ def gconnect():
         '-moz-border-radius: 150px;"> '
 
     flash("you are now logged in as %s" % login_session['username'])
-    print 'Gconnect: Done.'
+    # print 'Gconnect: Done.'
     return output
 
 
@@ -181,19 +181,19 @@ def getUserID(email):
 def gdisconnect():
     access_token = login_session.get('access_token')
     if access_token is None:
-        print 'Access Token is None'
+        # print 'Access Token is None'
         response = make_response(
             json.dumps('Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    print 'In gdisconnect access token is %s', access_token
-    print 'User name is: '
-    print login_session['username']
+    # print 'In gdisconnect access token is %s', access_token
+    # print 'User name is: '
+    # print login_session['username']
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
-    print 'result is '
-    print result
+    # print 'result is '
+    # print result
     if result['status'] == '200':
         del login_session['access_token']
         del login_session['gplus_id']
@@ -341,7 +341,9 @@ def editAuto(company_id, car_id):
     if request.method == 'POST':
         if request.form['name']:
             editCar.name = request.form['name']
+        if request.form['Specifications']
             editCar.Specifications = request.form['Specifications']
+        if request.form['price']
             editCar.price = request.form['price']
         session.add(editCar)
         session.commit()
